92832d71ee195e482dca3791402fe70d19e1ddb7
Анастасія Дзундза < >
2017-04-20 22:31:16 +0300

    creted class CreditCard

---
+++ Wallet/CreditCard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wallet
+{
+    class CreditCard
+    {
+        int creditAmount;
+
+        void showAmount() {
+
+            Console.Write("Thi is crdit card"+ creditAmount);
+        }
+       
+    }
+}
